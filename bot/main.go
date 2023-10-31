c7a189516adb6422501395a6ef65d83783997653
AsanSmailov <144768613+AsanSmailov@users.noreply.github.com>
2023-10-31 22:28:34 +0300

    Add files via upload

--- bot/main.go
+++ bot/main.go
@@ -9,9 +9,18 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+func check(chatids map[int64]string, Chat_ID int64) bool {
+	for user, _ := range chatids {
+		if user == Chat_ID {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
-	chatids := make([]int64, 0) //масив для хранения chatid,потом исправлю на map когда буду получать id github
-	var n int64
+	chatids := make(map[int64]string)
+	//chatids := make([]int64, 0) //масив для хранения chatid,потом исправлю на map когда буду получать id github
 	bot, err := tgbotapi.NewBotAPI("6320552220:AAFd90gcVVs0tLR4uXNPzcAL_thmjFAXt4U")
 	if err != nil {
 		log.Panic(err)
@@ -34,54 +43,52 @@ func main() {
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 		msg.ReplyToMessageID = update.Message.MessageID
 
-		switch update.Message.Text {
-		case "/start":
-			find := false
-			for _, user := range chatids {
-				if user == update.Message.Chat.ID {
-					find = true
-					break
-				}
-			}
-			if find == false {
-				msg.Text = "Привет! Я телеграмм бот c расписанием. \n Чтобы продолжить пользоватьсчя вам нужно зарегестрироваться"
-				bot.Send(msg)
-				client := http.Client{}
-				// Формируем строку запроса вместе с query string
-				requestURL := fmt.Sprintf("http://localhost:8080//auth?chatid=pdate.Message.From.UserName.users")
-				// Выполняем запрос на сервер. Ответ попадёт в переменную response
-				request, _ := http.NewRequest("GET", requestURL, nil)
-				response, _ := client.Do(request)
-				resBody, _ := io.ReadAll(response.Body) // Получаем тело ответа
-				defer response.Body.Close()
-				answer := string(resBody)
-				msg.Text = answer
-				bot.Send(msg)
-				//Здесь нужно добавить проверку зарегался ли пользователь
-				chatids = append(chatids, n)
-				chatids[n] = update.Message.Chat.ID
-				n++
+		if !check(chatids, update.Message.Chat.ID) {
+			msg.Text = "Привет! Я телеграмм бот c расписанием. \nЧтобы продолжить пользоватьсчя вам нужно зарегестрироваться"
+			bot.Send(msg)
+			client := http.Client{}
+			// Формируем строку запроса вместе с query string
+			requestURL := fmt.Sprintf("http://localhost:8080//auth?chatid=%d", update.Message.Chat.ID)
+			// Выполняем запрос на сервер. Ответ попадёт в переменную response
+			request, _ := http.NewRequest("GET", requestURL, nil)
+			response, _ := client.Do(request)
+			resBody, _ := io.ReadAll(response.Body) // Получаем тело ответ
+			msg.Text = string(resBody)
+			bot.Send(msg)
+			//Здесь нужно добавить проверку зарегался ли пользователь
+			http.HandleFunc("/gitid", nil)    // Обработчик отвечающий на запроса к /gitid
+			http.ListenAndServe(":8080", nil) // Запуск сервера на порту 8080
+			var r *http.Request
+			github_id := r.URL.Query().Get("githubid")
+			if github_id != "" {
+				chatids[update.Message.Chat.ID] = github_id
 				msg.Text = "Вы успешно зарегестрировались"
-			} else {
-				msg.Text = "Привет! Я телеграмм бот c расписанием. \n Нажми /help чтобы увидеть все команды."
 			}
 
-		case "/help":
-			msg.Text = "Список всех команд: ..."
-		case "toadmin":
-			//if role==admin{
-			msg.Text = "ссылка на стр панели администратора"
-			//}
-		case "Где следующая пара":
-			msg.Text = "..."
-		case "Расписание на сегодня":
-			msg.Text = "..."
-		case "Расписание на завтра":
-			msg.Text = "..."
-		case "c":
+			defer response.Body.Close()
+		} else {
+			switch update.Message.Text {
+			case "/start":
+				msg.Text = "Привет! Я телеграмм бот c расписанием. \nНажми /help чтобы увидеть все команды."
+			case "/help":
+				if check(chatids, update.Message.Chat.ID) {
+					msg.Text = "Список всех команд: ..."
+				}
+			case "toadmin":
+				//if role==admin{
+				msg.Text = "ссылка на стр панели администратора"
+				//}
+			case "Где следующая пара":
+				msg.Text = "..."
+			case "Расписание на сегодня":
+				msg.Text = "..."
+			case "Расписание на завтра":
+				msg.Text = "..."
+			case "c":
 
-		default:
-			msg.Text = "Я не понимаю, что вы хотите сказать."
+			default:
+				msg.Text = "Я не понимаю, что вы хотите сказать."
+			}
 		}
 
 		bot.Send(msg)
