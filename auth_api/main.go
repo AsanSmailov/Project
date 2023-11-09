62fca77a11cd35bb80a49fb8e32b838a964e3569
MaksimBaturin <144770548+MaksimBaturin@users.noreply.github.com>
2023-11-09 18:45:48 +0300

    Update main.go

--- auth_api/main.go
+++ auth_api/main.go
@@ -13,6 +13,9 @@ func main() {
 	// Регистрируем маршруты
 	router.HandleFunc("/auth", users.Auth)           // Бот делает запрос, к нему прикладывает chat_id, затем бот получает ссылку.
 	router.HandleFunc("/oauth", users.Oauth_handler) //Это для гитхаба
+	router.HandleFunc("/checkAbout", users.CheckAbout)
+	router.HandleFunc("/sendAbout", users.SendAbout).Methods("POST")
+	router.HandleFunc("/getRole", users.GetRole)
 
 	http.ListenAndServe(":8080", router)
 
