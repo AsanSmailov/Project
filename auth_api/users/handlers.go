90e608d39b51e6d22d20324c74e3bd231ae43417
MaksimBaturin <144770548+MaksimBaturin@users.noreply.github.com>
2023-11-11 20:02:13 +0300

    Update handlers.go

--- auth_api/users/handlers.go
+++ auth_api/users/handlers.go
@@ -4,42 +4,55 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type UserData struct {
 	Id int64 `json:"id"`
 }
 
+// Словарь сессий для авторизации
 var data = make(map[int64]int64)
 
+// Данные github oauth
 const (
 	CLIENT_ID     = "b783eb30a8893ae6852d"
 	CLIENT_SECRET = "660cc24cd1decbfaac1f1fe9793c523ff3954bf1"
 )
 
+// Handle функция, на запрос с tg id возвращает сгенерированную ссылку на github
 func Auth(rw http.ResponseWriter, req *http.Request) {
-	var tg_id int64
-	tg_id, _ = strconv.ParseInt(req.URL.Query().Get("chatid"), 10, 64)
-	var str string
-	str = strconv.FormatInt(tg_id, 10)
+	//Получаем данные из ссылки
+	tg_id, _ := strconv.ParseInt(req.URL.Query().Get("chatid"), 10, 64)
+	str := strconv.FormatInt(tg_id, 10)
+	//Создаём сессию
 	data[tg_id]++
+	//Генерируем и отдаём ссылку
 	var authURL string = "https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID + "&state=" + str
 	fmt.Fprintf(rw, "%s", authURL)
 }
 
+// Callback функция для github oauth, возвращает
 func Oauth_handler(rw http.ResponseWriter, req *http.Request) {
 	var responseHtml = "<html><body><h1>Вы не аутентифицированы!</h1></body></html"
 
+	//Получаем данные из ссылки
 	code := req.URL.Query().Get("code") // Достаем временный код из запроса
 	tg_id, _ := strconv.ParseInt(req.URL.Query().Get("state"), 10, 64)
 	_, ok := data[tg_id]
 
+	//Проверяем наличие временного кода и сессии
 	if code != "" && ok {
+		//Обмениваем временный код на accesstoken
 		accessToken := getAccessToken(code)
+		//Получаем github id
 		data[tg_id] = getUserData(accessToken)
 
 		if !checkData(data[tg_id], tg_id) { //Проверяем существует ли док с таким id, если нет, то создаём док.
@@ -47,6 +60,7 @@ func Oauth_handler(rw http.ResponseWriter, req *http.Request) {
 		}
 		responseHtml = "<html><body><h1>Вы аутентифицированы!</h1></body></html>"
 
+		//Создаём запрос callback боту, отправляем ему github id и tg id
 		url := "http://localhost:8081/gitid?githubid=" + strconv.FormatInt(data[tg_id], 10) + "&chatid=" + strconv.FormatInt(tg_id, 10)
 		requesturl := fmt.Sprintf(url)
 		client := http.Client{}
@@ -59,10 +73,13 @@ func Oauth_handler(rw http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(rw, responseHtml)
 }
 
+// Handle функция, принимает tg id, возвращает bool в зависимости указана ли имя или группа
 func CheckAbout(rw http.ResponseWriter, req *http.Request) {
+	//Получаем данные из ссылки
 	tg_id, _ := strconv.ParseInt(req.URL.Query().Get("chatid"), 10, 64)
-	user := getData(tg_id)
-
+	//Получаем структуру user
+	user := getData(tg_id, "tg_id")
+	// Проверяем наличия имени или группы и отправляем ответ
 	if user.About.Group == "" && user.About.FullName == "" {
 		fmt.Fprintf(rw, "%t", false)
 	} else {
@@ -70,21 +87,74 @@ func CheckAbout(rw http.ResponseWriter, req *http.Request) {
 	}
 }
 
+// Handle функция, принимает id, информацию, которую надо изменить, тип информации, возвращает bool
 func SendAbout(rw http.ResponseWriter, req *http.Request) {
+	//Получаем данные
 	tg_id, _ := strconv.ParseInt(req.FormValue("chatid"), 10, 64)
 	data := req.FormValue("data")
 	datatype := req.FormValue("datatype")
-
-	log.Print("sendAbout logs:", tg_id, " ", data, " ", datatype)
+	//Отвечаем, используя inputData()
 	fmt.Fprintf(rw, "%t", inputData(tg_id, data, datatype))
 }
 
+// Handle функция, принимает id, в ответ отдаёт роль пользователя
 func GetRole(rw http.ResponseWriter, req *http.Request) {
+	//Получаем данные
 	tg_id, _ := strconv.ParseInt(req.URL.Query().Get("chatid"), 10, 64)
-	user := getData(tg_id)
-	fmt.Print("GetRole log:", user.Role)
-
+	//Получаем структуру user
+	user := getData(tg_id, "tg_id")
+	//Отвечаем
 	fmt.Fprintf(rw, "%s", user.Role)
+}
+
+// Handle функция, принимает id и действие, возвращает сгенерированный jwt token
+func JWTschedule(rw http.ResponseWriter, req *http.Request) {
+	//Получаем данные
+	github_id, _ := strconv.ParseInt(req.FormValue("gitid"), 10, 64)
+	action := string(req.FormValue("action"))
+	//Генерируем секрет
+	SECRET := randJWTSecret(16)
+
+	//Отправляем секретный код модулю расписания
+	client := http.Client{}
+	requesturl := "http://localhost:8082/getSecret"
+
+	form := url.Values{}
+	form.Add("SECRET", SECRET)
+	request, _ := http.NewRequest("POST", requesturl, strings.NewReader(form.Encode()))
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	response, _ := client.Do(request)
+	defer response.Body.Close()
+
+	//Получаем данные
+	user := getData(github_id, "github_id")
+	//Формируем токен
+	tokenExpiresAt := time.Now().Add(time.Second * time.Duration(60))
+	payload := jwt.MapClaims{
+		"action":     action,
+		"full_name":  user.About.FullName,
+		"group":      user.About.Group,
+		"expires_at": tokenExpiresAt,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	//Подписываем токен секретным кодом
+	tokenString, err := token.SignedString([]byte(SECRET))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(rw, "%s", tokenString)
+}
+
+// Символы для генерации токена
+var charset = []byte("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+// Генерируем случайную строку размером n символов
+func randJWTSecret(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
 }
 
 // Для Oauth_handler
