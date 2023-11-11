e60809825d10f387ec083131d46f1f3a9935c66e
MaksimBaturin <144770548+MaksimBaturin@users.noreply.github.com>
2023-11-11 20:01:45 +0300

    Update storage.go

--- auth_api/users/storage.go
+++ auth_api/users/storage.go
@@ -21,32 +21,39 @@ type User struct {
 	About    About  `bson:"about"`
 }
 
+// Функция, проверяет наличие документа с github и tg id
 func checkData(githubID int64, tgID int64) bool {
+	//Соединяемся с MongoDB
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 	err = client.Connect(context.TODO())
 	err = client.Ping(context.TODO(), nil)
-
+	//Подключаемся к коллекции user
 	collection := client.Database("UsersDB").Collection("user")
-
+	//Формируем фильтр поиска
 	filter := bson.D{{"github_id", githubID}, {"tg_id", tgID}}
 	log.Print(tgID, " ", githubID)
 	var result User
-
+	//Ищем документ, если документа нет, то возвращает false
 	err = collection.FindOne(context.TODO(), filter).Decode(&result)
 	if err != nil {
 		err = client.Disconnect(context.TODO())
 		return false
 	} else {
+		err = client.Disconnect(context.TODO())
 		return true
 	}
 }
 
+// Функция, создания нового документа
 func register(githubID int64, tgID int64) {
+	//Соединяемся с MongoDB
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 	err = client.Connect(context.TODO())
 	err = client.Ping(context.TODO(), nil)
-
+	//Подключаемся к коллекции user
 	collection := client.Database("UsersDB").Collection("user")
+
+	//Формируем пользователя
 	var user User
 	log.Print(tgID, " ", githubID)
 	user.GithubID = githubID
@@ -54,25 +61,28 @@ func register(githubID int64, tgID int64) {
 	user.Role = "student"
 	user.About.FullName = ""
 	user.About.Group = ""
-
+	//Создаём новый документ
 	_, err = collection.InsertOne(context.TODO(), user)
 	if err != nil {
 		log.Fatal(err)
 	}
+	//Отключаемся от MongoDB
 	err = client.Disconnect(context.TODO())
 }
 
-func getData(tgID int64) User {
+// Функция, по id возвращает данные пользователя
+func getData(ID int64, IDtype string) User {
+	//Соединяемся с MongoDB
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 	err = client.Connect(context.TODO())
 	err = client.Ping(context.TODO(), nil)
-
+	//Подключаемся к коллекции user
 	collection := client.Database("UsersDB").Collection("user")
-
-	filter := bson.D{{"tg_id", tgID}}
+	//Формируем фильтр поиска
+	filter := bson.D{{IDtype, ID}}
 
 	var result User
-
+	//Получаем данные
 	err = collection.FindOne(context.TODO(), filter).Decode(&result)
 	if err != nil {
 		log.Fatal(err)
@@ -81,22 +91,27 @@ func getData(tgID int64) User {
 	return result
 }
 
+// Функция, изменяет данные в документы, на вход принимает tg id, данные, тип данных, возвращает bool
 func inputData(tgID int64, data string, datatype string) bool {
+	//Соединяемся с MongoDB
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 	err = client.Connect(context.TODO())
 	err = client.Ping(context.TODO(), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	//Подключаемся к коллекции user
 	collection := client.Database("UsersDB").Collection("user")
+	//Формируем фильтр поиска
 	filter := bson.D{{"tg_id", tgID}}
+	//Формируем фильтр данных, которые будем изменять
 	var replacement bson.D
 	if datatype != "role" {
 		replacement = bson.D{{"$set", bson.D{{"about." + datatype, data}}}}
 	} else {
 		replacement = bson.D{{datatype, data}}
 	}
+	//Изменяем данные
 	_, err1 := collection.UpdateOne(context.TODO(), filter, replacement)
 	if err1 != nil {
 		err1 = client.Disconnect(context.TODO())
