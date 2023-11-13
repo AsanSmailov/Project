cd6dc1d10bd4309a1f905a010e293f157d036947
MaksimBaturin <144770548+MaksimBaturin@users.noreply.github.com>
2023-11-13 18:35:21 +0300

    Update main.go

--- auth_api/main.go
+++ auth_api/main.go
@@ -17,7 +17,7 @@ func main() {
 	router.HandleFunc("/sendAbout", users.SendAbout).Methods("POST")         //Изменение имени или группы
 	router.HandleFunc("/getRole", users.GetRole)                             //Отправка роли пользователя
 	router.HandleFunc("/getJWT/schedule", users.JWTschedule).Methods("POST") //Формирование jwt token для модуля Расписания
-
+	router.HandleFunc("/getJWT/admin", users.JWTadmin).Methods("POST")
 	http.ListenAndServe(":8080", router)
 
 }
