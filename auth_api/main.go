83cc9375de6ea06d6f698df52a739e0111f87c3c
MaksimBaturin <144770548+MaksimBaturin@users.noreply.github.com>
2023-11-14 20:20:52 +0300

    Update main.go

--- auth_api/main.go
+++ auth_api/main.go
@@ -14,10 +14,13 @@ func main() {
 	router.HandleFunc("/auth", users.Auth)                                   //Формирование ссылки github
 	router.HandleFunc("/oauth", users.Oauth_handler)                         //github callback
 	router.HandleFunc("/checkAbout", users.CheckAbout)                       //Проверяет наличия имени или группы
-	router.HandleFunc("/sendAbout", users.SendAbout).Methods("POST")         //Изменение имени или группы
-	router.HandleFunc("/getRole", users.GetRole)                             //Отправка роли пользователя
+	router.HandleFunc("/updateData", users.UpdateData).Methods("POST")       //Изменение данных пользователя. Функция принимает tg_id, данные, тип данных(role, group, full_name)
+	router.HandleFunc("/getRole", users.GetRole)                             //Отправка роли пользователя по его tg_id
 	router.HandleFunc("/getJWT/schedule", users.JWTschedule).Methods("POST") //Формирование jwt token для модуля Расписания
-	router.HandleFunc("/getJWT/admin", users.JWTadmin).Methods("POST")
+	router.HandleFunc("/getJWT/admin", users.JWTadmin).Methods("POST")       //Формирование jwt token для модуля Администрирования
+
+	router.HandleFunc("/getAllUsers", users.GetAllUsers).Methods("POST") //В json формате передаёт информацию о всех пользователей
+	router.HandleFunc("/DeleteUser", users.DeleteUser).Methods("POST")   //Удаление пользователя по его gihub_id
 	http.ListenAndServe(":8080", router)
 
 }
